03a17df9c0fd6e08505374e2fe9ffbc4efa873bc
Johnsensei <johngale@Minas-Tirith.local>
2019-07-13 19:01:14 -0500

    0.0.0
    Initial Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float timeLeft = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (SceneManager.GetActiveScene ().buildIndex == 1) {
+			timeLeft -= Time.deltaTime;
+			print ("Time remaining " + timeLeft);
+		}
+
+		if (timeLeft <= 0f) {
+			LoadNextScene ();
+		}
+
+	
+	}
+
+	public void LoadNextScene(){
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+
+	}
+
+	public void LoadPreviousScene(){
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex - 1);
+
+	}
+
+}
---
+++ Assets/Scripts/ScoreTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour {
+
+	Collider expectedCollider;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExpectCollider (Collider collider){
+
+		expectedCollider = collider;
+	}
+
+	void OnTriggerEnter(Collider otherCollider){
+		if (otherCollider == expectedCollider) {
+			ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper> ();
+			scoreKeeper.IncrementScore (1);
+			AudioSource source = GetComponent<AudioSource> ();
+			source.Play ();
+		
+		}
+
+	}
+
+}
